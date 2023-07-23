97adbb2f673d38804f7cb432fd72e44f1c9eb344
Harish <82930803+Sivaharish12@users.noreply.github.com>
2023-07-23 09:28:51 +0530

    Initial commit

---
+++ src/data.js
@@ -0,0 +1,25 @@
+let shopItemsData=[{
+    id:"qwer",
+    name:"Formal Shirt",
+    price:100,
+    desc:"it is a formal wear.",
+    img:"img/shirt.jpg"
+},{
+    id:"asdf",
+    name:"Tshirt",
+    price:100,
+    desc:"it is a casual wear.",
+    img:"img/Tshirt.jpg"
+},{
+    id:"zxcv",
+    name:"chain",
+    price:100,
+    desc:"it is a custom chain used for the decaration",
+    img:"img/chain.jpg"
+},{
+    id:"edfc",
+    name:"watch",
+    price:100,
+    desc:"A branded watch used to show the time",
+    img:"img/watch.jpg"
+}];
